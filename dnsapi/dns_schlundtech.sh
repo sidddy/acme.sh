e066fcbbe338eff418ef2111b95d9936e1695d8d
Holger Böhnke <holger.boehnke@amarin.de>
2017-08-11 17:46:38 +0200

    add xml fragments for schlundtech provider

--- dnsapi/dns_schlundtech.sh
+++ dnsapi/dns_schlundtech.sh
@@ -33,3 +33,60 @@ dns_myapi_rm() {
 }
 
 ####################  Private functions below ##################################
+
+read -r -d '' xmladd <<EOFXML
+<?xml version="1.0" encoding="utf-8"?>
+<request>
+  <auth>
+    <user>0000000</user>
+    <password>xxxxxxxxxxxxxxxxxxxx</password>
+    <context>10</context>
+  </auth>
+  <task>
+    <code>0202001</code>
+    <default>
+      <rr_add>
+        <name>{subdomain}</name>
+        <type>TXT</type>
+        <value>{value}</value>
+        <ttl>60</ttl>
+      </rr_add>
+    </default>
+    <zone>
+      <name>{domain}</name>
+    </zone>
+  </task>
+</request>
+EOFXML
+
+
+
+read -r -d '' xmlrm <<EOFXML
+<?xml version="1.0" encoding="utf-8"?>
+<request>
+  <auth>
+    <user>0000000</user>
+    <password>xxxxxxxxxxxxxxxxxxxx</password>
+    <context>10</context>
+  </auth>
+  <task>
+    <code>0202001</code>
+    <default>
+      <rr_rem>
+        <name>{subdomain}</name>
+        <type>TXT</type>
+        <value>{value}</value>
+      </rr_rem>
+    </default>
+    <zone>
+      <name>{domain}</name>
+    </zone>
+  </task>
+</request>
+EOFXML
+
+echo "----------------"
+echo "$xmladd" | sed 's/{domain}/example.com/;s/{subdomain}/acmedom/;s/{value}/wurst/' 
+echo "----------------"
+echo "$xmlrm" | sed 's/{domain}/example.com/;s/{subdomain}/acmedom/;s/{value}/wurst/' 
+echo "----------------"
